2433233c7854a5c92f9ed3dcc53286d8a7e92395
Gilcimar Soares Leite <leite.gilcimar@gmail.com>
2022-11-21 16:26:01 -0300

    Adicionado o status 201 no retorno do método buscar na classe CozinhaController.

--- src/main/java/br/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import br.com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import br.com.algaworks.algafood.domain.model.Cozinha;
 import br.com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -26,6 +27,7 @@ public class CozinhaController {
         return new CozinhasXmlWrapper(cozinhaRepository.todas());
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @GetMapping("/{cozinhaId}")
     public Cozinha buscar(@PathVariable Long cozinhaId){
         return cozinhaRepository.porId(cozinhaId);
