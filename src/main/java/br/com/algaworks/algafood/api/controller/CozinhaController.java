1a429ccbedf287a460602e16613d23a08b1bc0da
Gilcimar Soares Leite <leite.gilcimar@gmail.com>
2022-11-21 14:11:56 -0300

    Implementado a busca de cozinha por id e removendo a compatibilidade do retorno do recurso listar retornar em xml.

--- src/main/java/br/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,26 +5,25 @@ import br.com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/cozinhas") //, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping("/cozinhas")
 public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public List<Cozinha> listar1(){
+    @GetMapping
+    public List<Cozinha> listar(){
         return cozinhaRepository.todas();
     }
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Cozinha> listar2(){
-        return cozinhaRepository.todas();
+
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId){
+        return cozinhaRepository.porId(cozinhaId);
     }
+
 }
