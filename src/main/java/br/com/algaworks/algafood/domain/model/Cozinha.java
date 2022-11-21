6f1cd90e16381e034a27278d65bf49bbd103ee39
Gilcimar Soares Leite <leite.gilcimar@gmail.com>
2022-11-21 14:22:40 -0300

    Customizando o retorno json das chaves na classe Cozinha

--- src/main/java/br/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/br/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,11 +1,15 @@
 package br.com.algaworks.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.Objects;
 
+@JsonRootName("cozinha")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -16,6 +20,8 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //@JsonIgnore // Remove a propriedade no retorno
+    //@JsonProperty("titulo") // Moda o nome da propriedade para titulo
     @Column(nullable = false)
     private String nome;
 
